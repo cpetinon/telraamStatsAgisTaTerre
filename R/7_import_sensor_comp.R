7af3037cf11da6f1e6bf51cc6c7b833c286549ba
UCaromel <caromelulysse@gmail.com>
2023-08-11 12:50:37 +0200

    corrected import for completed data

--- R/7_import_sensor_comp.R
+++ R/7_import_sensor_comp.R
@@ -15,27 +15,25 @@
 #' @export
 #'
 import_sensor_comp <- function(sensor_ids,sensor_comp_names){
-  data <- data.frame()
-  data <- map_dfr(sensor_comp_names, ~ {
+  res <- data.frame()
+  res <- map_dfr(sensor_comp_names, ~ {
     file <- paste0('data/', .x, '.RData')
     if (file.exists(file)) {
-      get(load(file, header = TRUE, sep = ";", dec = ","))
+      data <- get(load(file, header = TRUE, sep = ";", dec = ","))
+      car <- data$car
+      car_speed_hist_0to120plus <- data$car_speed_hist_0to120plus
+      car_speed_hist_0to70plus <- data$car_speed_hist_0to70plus
+
+      data_na <- data %>% filter(is.na(car)) %>% mutate(car_speed_hist_0to120plus = NA,
+                                                        car_speed_hist_0to70plus = NA)
+      data_sana <- data %>% filter(!is.na(car)) %>%
+        mutate(car_speed_hist_0to70plus = convert_string_to_list(car_speed_hist_0to70plus),
+               car_speed_hist_0to120plus = convert_string_to_list(car_speed_hist_0to120plus))
+      data <- rbind(data_na, data_sana) %>% arrange(date)
+      # data$date <- ymd_hms(data$date)
     } else {
       NULL
     }
   })
-
-  car <- data$car
-  car_speed_hist_0to120plus <- data$car_speed_hist_0to120plus
-  car_speed_hist_0to70plus <- data$car_speed_hist_0to70plus
-
-  data_na <- data %>% filter(is.na(car)) %>% mutate(car_speed_hist_0to120plus = NA,
-                                                  car_speed_hist_0to70plus = NA)
-  data_sana <- data %>% filter(!is.na(car)) %>%
-    mutate(car_speed_hist_0to70plus = convert_string_to_list(car_speed_hist_0to70plus),
-           car_speed_hist_0to120plus = convert_string_to_list(car_speed_hist_0to120plus))
-  data <- rbind(data_na, data_sana) %>% arrange(date)
-
-  # data$date <- ymd_hms(data$date)
-  return(data)
+  return(res)
 }
