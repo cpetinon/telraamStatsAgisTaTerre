f0299c5b2bb60ed15dc4c8055c3d60518d561da9
KevinKen237 <kevinkenang1@gmail.com>
2024-07-12 10:03:29 +0200

    Update plot_diagram_envelope

--- R/9_plot_diagram_envelope.R
+++ R/9_plot_diagram_envelope.R
@@ -59,14 +59,12 @@ calculate_axes<-function(enriched_data)
 #' calculate_characteristic(traffic,
 #'   direction_choice='lft')
 
-calculate_characteristic<- function (enriched_data,
-                                     direction_choice=NULL)
+calculate_characteristic<- function (enriched_data, direction_choice=NULL)
 {
   if(is.null(direction_choice) || !("speed_hist_car_lft" %in% colnames(enriched_data)))
   {
     density <- sum(enriched_data$veh_km,na.rm = TRUE)/length(enriched_data$veh_km)
     speed<- sum(enriched_data$km_h,na.rm = TRUE)/length(enriched_data$km_h)
-    flow<- sum(enriched_data$veh_h,na.rm = TRUE)/length(enriched_data$veh_h)
   }
 
 
@@ -74,18 +72,16 @@ calculate_characteristic<- function (enriched_data,
   {
     density<-sum(enriched_data$veh_km_lft,na.rm = TRUE)/length(enriched_data$veh_km_lft)
     speed<-sum(enriched_data$km_h_lft,na.rm = TRUE)/length(enriched_data$km_h_lft)
-    flow<- sum(enriched_data$veh_h_lft,na.rm = TRUE)/length(enriched_data$veh_h_lft)
   }
 
   else
   {
     density<-sum(enriched_data$veh_km_rgt,na.rm = TRUE)/length(enriched_data$veh_km_rgt)
     speed<-sum(enriched_data$km_h_rgt,na.rm = TRUE)/length(enriched_data$km_h_rgt)
-    flow<- sum(enriched_data$veh_h_rgt,na.rm = TRUE)/length(enriched_data$veh_h_rgt)
   }
 
 
-  return(list(flow=flow,speed=speed,density=density))
+  return(list(speed=speed,density=density))
 
 }
 
@@ -119,8 +115,7 @@ calculate_data_dimensionless<- function (enriched_data,
     enriched_data <- enriched_data %>%
       mutate (
         veh_km_adim=veh_km/list_charac$density,
-        km_h_adim=km_h/list_charac$speed,
-        veh_h_adim= veh_h/list_charac$flow)
+        km_h_adim=km_h/list_charac$speed)
   }
 
   else if (direction_choice=='lft')
@@ -128,8 +123,7 @@ calculate_data_dimensionless<- function (enriched_data,
     enriched_data <- enriched_data %>%
       mutate (
         veh_km_lft_adim=veh_km_lft/list_charac$density,
-        km_h_lft_adim=km_h_lft/list_charac$speed,
-        veh_h_lft_adim=veh_h_lft/list_charac$flow)
+        km_h_lft_adim=km_h_lft/list_charac$speed)
   }
 
   else
@@ -137,8 +131,7 @@ calculate_data_dimensionless<- function (enriched_data,
     enriched_data <- enriched_data %>%
       mutate (
         veh_km_rgt_adim=veh_km_rgt/list_charac$density,
-        km_h_rgt_adim=km_h_rgt/list_charac$speed,
-        veh_h_rgt_adim=veh_h_rgt/list_charac$flow)
+        km_h_rgt_adim=km_h_rgt/list_charac$speed)
   }
 
   return(enriched_data)
@@ -488,17 +481,17 @@ filter_demand_user<-function (enriched_data,
 
 
 plot_diagram_envelope <- function (enriched_data,
-                                  date_range = NULL,
-                                  weekday_choice = NULL,
-                                  hour_choice = NULL,
-                                  vacation_choice=NULL,
-                                  holiday_choice=NULL,
-                                  segments = NULL,
-                                  direction_choice=NULL,
-                                  NumberOfSlope=50,
-                                  NumberOfOrdinate=45,
-                                  percent1=c(95,99),
-                                  percent2=c(95,99))
+                                   date_range = NULL,
+                                   weekday_choice = NULL,
+                                   hour_choice = NULL,
+                                   vacation_choice=NULL,
+                                   holiday_choice=NULL,
+                                   segments = NULL,
+                                   direction_choice=NULL,
+                                   NumberOfSlope=50,
+                                   NumberOfOrdinate=45,
+                                   percent1=c(95,99),
+                                   percent2=c(95,99))
 {
   enriched_data<-filter_demand_user(enriched_data,
                                     segments,
@@ -549,9 +542,9 @@ plot_diagram_envelope <- function (enriched_data,
 
     # Affiche le diagramme et les droites
     graphique<-plot_lines(df,
-                    list_final_1,
-                    list_final_2,
-                    direction_choice)
+                          list_final_1,
+                          list_final_2,
+                          direction_choice)
 
     plot(graphique$linear)
     plot(graphique$parabolic)
@@ -622,28 +615,34 @@ plot_lines <- function (enriched_data,
   #Plot the fundamental diagram and its envelope
   graphique<-list(linear=NULL,parabolic=NULL)
 
-  graphique$linear<-ggplot(data = enriched_data, mapping = aes(x = abscissa, y = ordinate1, color = 'red')) +
+  graphique$linear<-ggplot(data = enriched_data, mapping = aes(x = abscissa, y = ordinate1)) +
     geom_point(pch = 20) +
-    labs(x = 'Density', y = 'Speed', title = paste('Segment :', enriched_data$segment_fullname[1]))+
-    geom_line(mapping=aes(x=abscissa,y=list_final_1$a*abscissa+list_final_1$b),color='black')+
-    geom_line(mapping=aes(x=abscissa,y=list_final_2$a*abscissa+list_final_2$b),color='black')+
-    geom_point(aes(x = x_inter, y = y_inter_1), color = "green", size = 2) +
-    geom_text(aes(x = x_inter, y = y_inter_1,hjust=-1, label = paste("x =", sprintf("%.2f", x_inter))))+
-    geom_point(aes(x = x_lim_1, y = 0), color = "green", size = 2) +
-    geom_text(aes(x = x_lim_1, y = 0,hjust=1, label = paste("x =", sprintf("%.2f", x_lim_1))))+
+    labs(x = 'Density (veh/km)', y = 'Speed (km/h)', title = paste('Segment :', enriched_data$segment_fullname[1]))+
+    geom_line(mapping=aes(x=abscissa,y=list_final_1$a*abscissa+list_final_1$b),color='red')+
+    geom_line(mapping=aes(x=abscissa,y=list_final_2$a*abscissa+list_final_2$b),color='blue')+
+
+    geom_point(aes(x = x_inter, y = y_inter_1),shape=15, color = "orange", size = 3) +
+    geom_text(aes(x = x_inter, y = y_inter_1,hjust=-0.5, label = paste("x =", sprintf("%.2f", x_inter))))+
+
+    geom_point(aes(x = x_lim_1, y = 0),shape=15, color = "orange", size = 3) +
+    geom_text(aes(x = x_lim_1, y = 0,hjust=1.4, label = paste("x =", sprintf("%.2f", x_lim_1))))+
+
     coord_cartesian(xlim =c(0, x_lim_1), ylim = c(0, max(ordinate1)))
 
 
   #Plot the other one
-  graphique$parabolic<-ggplot(data = enriched_data, mapping = aes(x = abscissa, y = ordinate2, color = 'red')) +
+  graphique$parabolic<-ggplot(data = enriched_data, mapping = aes(x = abscissa, y = ordinate2)) +
     geom_point(pch = 20) +
-    labs(x = 'Density', y = 'Flow', title = paste('Segment :', enriched_data$segment_fullname[1]))+
-    geom_line(mapping=aes(x=abscissa,y=list_final_1$a*abscissa*abscissa+list_final_1$b*abscissa),color='black')+
-    geom_line(mapping=aes(x=abscissa,y=list_final_2$a*abscissa*abscissa+list_final_2$b*abscissa),color='black')+
-    geom_point(aes(x = x_inter, y = y_inter_2), color = "green", size = 2) +
-    geom_text(aes(x = x_inter, y = y_inter_2,hjust=2, label = paste("y =", sprintf("%.2f", y_inter_2))))+
-    geom_point(aes(x = x_lim_2, y = y_lim_2), color = "green", size = 2) +
-    geom_text(aes(x = x_lim_2, y = y_lim_2,vjust=-2, label = paste("y =", sprintf("%.2f", y_lim_2))))+
+    labs(x = 'Density (veh/km)', y = 'Flow (veh/h)', title = paste('Segment :', enriched_data$segment_fullname[1]))+
+    geom_line(mapping=aes(x=abscissa,y=list_final_1$a*abscissa*abscissa+list_final_1$b*abscissa),color='red')+
+    geom_line(mapping=aes(x=abscissa,y=list_final_2$a*abscissa*abscissa+list_final_2$b*abscissa),color='blue')+
+
+    geom_point(aes(x = x_inter, y = y_inter_2),shape=15, color = "orange", size = 3) +
+    geom_text(aes(x = x_inter, y = y_inter_2,hjust=1.5, label = paste("y =", sprintf("%.2f", y_inter_2))))+
+
+    geom_point(aes(x = x_lim_2, y = y_lim_2),shape=15, color = "orange", size = 3) +
+    geom_text(aes(x = x_lim_2, y = y_lim_2,hjust=-0.5, label = paste("y =", sprintf("%.2f", y_lim_2))))+
+
     coord_cartesian(xlim =c(0, max(abscissa)), ylim = c(0, max(ordinate2)))
 
   return(graphique)
@@ -796,7 +795,7 @@ retrieve_missing_hours<-function(enriched_data,
                                  ifelse(month(enriched_data$date) %in% c(6,7,8), "Summer",
                                         ifelse(month(enriched_data$date) %in% c(9,10,11), "Autumn", "Winter")))
 
-  df_season<-enriched_data %>% group_by(segment_id,season,hour) %>% summarise(condition=any(car!=0 & uptime>uptime_choice))
+  df_season<-enriched_data %>% group_by(segment_id,season,hour) %>% summarise(condition=any(car!=0 & uptime>uptime_choice), .groups="keep")
 
   enriched_data <- enriched_data %>% semi_join(df_season %>% filter(condition), by = c("segment_id","season", "hour"))
 
