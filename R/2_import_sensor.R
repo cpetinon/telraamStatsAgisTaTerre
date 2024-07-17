d8cb012a53a6055e159ae2f8149dc52e0074b1cf
KevinKen237 <kevinkenang1@gmail.com>
2024-07-17 22:09:07 +0200

    update import_sensors to introduice boulevard leannec1

--- R/2_import_sensor.R
+++ R/2_import_sensor.R
@@ -19,12 +19,12 @@ import_sensor <- function(list_sensor,
                         sensor_names = c("Burel-01","Leclerc-02","ParisMarche-03","rueVignes-04","ParisArcEnCiel-05","RteVitre-06",
                                          "RueGdDomaine-07","StDidierNord-08","rueVallee-09","StDidierSud-10","RuePrieure-11",
                                          "RueCottage-12","RueVeronniere-13","RueDesEcoles-14","RueManoirs-15","RueToursCarree-16",
-                                         "PlaceHotelDeVille-17","BoulevardLiberte-18", "AvenueDesPlatanes-19", "BoulevardLeannec2-21", "RuePasteur-22", "RueRandonneurs-23"),
+                                         "PlaceHotelDeVille-17","BoulevardLiberte-18", "AvenueDesPlatanes-19", "BoulevardLeannec1-20", "BoulevardLeannec2-21", "RuePasteur-22", "RueRandonneurs-23"),
 
                         sensor_ids = c(9000002156, 9000001906, 9000001618,9000003090,9000002453,9000001844,
                                        9000001877,9000002666,9000002181,9000002707,9000003703,
                                        9000003746,9000003775,9000003736,9000004971,9000004130,
-                                       9000004042,9000004697, 9000006227, 9000006207,9000004019, 9000006807)
+                                       9000004042,9000004697, 9000006227, 9000006552, 9000006207,9000004019, 9000006807)
                         ){
 
   data <- data.frame()
