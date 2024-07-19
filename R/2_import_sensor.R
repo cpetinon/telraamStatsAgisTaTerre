c2c6d3e7ca36033651a7431a87a32f11bcf39905
« Slazz26 » <paul.vallee1@icloud.com>
2024-07-19 08:57:45 +0200

    modif Import Sensor

--- R/2_import_sensor.R
+++ R/2_import_sensor.R
@@ -35,9 +35,9 @@ import_sensor <- function(list_sensor,
       # we select the data that we don't consider null (arbitrary choice)
       import <- load(file)
       import <- get(import)
-      import <- import %>% filter(
-                                           .data$heavy_lft + .data$car_lft + .data$pedestrian_lft + .data$bike_lft +
-                                             .data$heavy_rgt + .data$car_rgt + .data$pedestrian_rgt + .data$bike_rgt >0)
+     # import <- import %>% filter(.data$uptime > 0.5 ,
+      #                                     .data$heavy_lft + .data$car_lft + .data$pedestrian_lft + .data$bike_lft +
+      #                                       .data$heavy_rgt + .data$car_rgt + .data$pedestrian_rgt + .data$bike_rgt >0)
       #import$car_speed_hist_0to70plus <-  convert_string_to_list(import$car_speed_hist_0to70plus)
       #import$car_speed_hist_0to120plus <- convert_string_to_list(import$car_speed_hist_0to120plus)
       import$date <- as.POSIXct(import$date)
@@ -72,3 +72,10 @@ convert_string_to_list <- function(vector){
     return(numeric_elements)
   })
 }
+
+
+data_choice <- reactiveValues(
+  sensors = NULL,
+  raw_data = tibble(),  # Données brutes complètes
+  filtered_data = tibble()  # Données filtrées
+)
