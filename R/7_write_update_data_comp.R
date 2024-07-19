e0c1f83962bc4d65e30c9c445dc1442f61acd963
KevinKen237 <kevinkenang1@gmail.com>
2024-07-19 12:39:57 +0200

    update write_update_data_comp add new sensors

--- R/7_write_update_data_comp.R
+++ R/7_write_update_data_comp.R
@@ -20,11 +20,11 @@ write_update_data_comp <- function(id_sensor, date1, date2,
                                    sensor_names = c("Burel-01","Leclerc-02","ParisMarche-03","rueVignes-04","ParisArcEnCiel-05","RteVitre-06",
                                                     "RueGdDomaine-07","StDidierNord-08","rueVallee-09","StDidierSud-10","RuePrieure-11",
                                                     "RueCottage-12","RueVeronniere-13","RueDesEcoles-14","RueManoirs-15","RueToursCarree-16",
-                                                    "PlaceHotelDeVille-17","BoulevardLiberte-18"),
+                                                    "PlaceHotelDeVille-17","BoulevardLiberte-18", "AvenueDesPlatanes-19", "BoulevardLeannec1-20", "BoulevardLeannec2-21", "RuePasteur-22", "RueRandonneurs-23"),
                                    sensor_ids = c(9000002156, 9000001906, 9000001618,9000003090,9000002453,9000001844,
                                                   9000001877,9000002666,9000002181,9000002707,9000003703,
                                                   9000003746,9000003775,9000003736,9000004971,9000004130,
-                                                  9000004042,9000004697),
+                                                  9000004042,9000004697, 9000006227, 9000006552, 9000006207,9000004019, 9000006807),
                                    vacations = NULL,
                                    public_holidays = NULL
                                   ){
